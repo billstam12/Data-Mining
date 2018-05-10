f1e65fd5f58464b2bed9a89d5158a29a5059a318
billstam12 <billstam12@gmail.com>
2018-05-10 17:16:02 +0300

    Some changes in 2

--- ergasia2/dtw_nn.py
+++ ergasia2/dtw_nn.py
@@ -1,6 +1,7 @@
 import pandas as pd 
 from ast import literal_eval
 import dtw
+import numpy as np 
 import gmplot
 from math import radians, cos, sin, asin, sqrt
 
@@ -9,15 +10,12 @@ AVG_EARTH_RADIUS = 6371 #km
 train_set = pd.read_csv('datasets/train_set.csv', 
 						converters = {"Trajectory": literal_eval},
 						index_col='tripId')
-test_set = pd.read_csv('datasets/test_set_a1.csv', 
-						converters = {"Trajectory": literal_eval},
-						index_col='tripId')
+
+test_set = pd.read_csv('datasets/test_set_a1.csv', sep =';', converters = {"Trajectory": literal_eval})
 						
-def haversine(point1, point2):
+def haversine(lon1,lat1,lon2,lat2):
 	
-	lon1, lat1 = point1
-	lon2, lat2 = point2
-	
+		
 	lon1, lat1, lon2, lat2, = map(radians, (lon1, lat1, lon2, lat2))
 	
 	#calculate haversine distance
@@ -28,11 +26,53 @@ def haversine(point1, point2):
 	d = sin(lat * 0.5) ** 2 + cos(lat1) * cos(lat2) * sin(lon * 0.5) ** 2
 	h = 2* AVG_EARTH_RADIUS * asin(sqrt(d))
 	
-	return h 
+	return h
+
+def DTW(search, target):
+	n = len(search)
+	m = len(target)
+	dtw = numpy.zeros((n,m))
 	
+	for i in range(1,n):
+		dtw[i][0] = float("inf")
+	for i in range(1,m):
+		dtw[0][i] = float("inf")
+	dtw[0][0] = 0
+	
+	
+	for i in range(1, n):
+		for j in range(1, m):
+			cost = haversine(search[i][1], search[i][2], target[j][1], target[j][2])
+			dtw [i][j] = cost + min(dtw[i - 1][j], dtw[i][j - 1], dtw[i - 1][j - 1])
+			
+	return dtw[n-1][m-1]
+
+
 traj = train_set['Trajectory']
+train_traj = traj.values
 
-traj_list = traj.values
+test_traj = test_set['Trajectory']
+# Here we iterate through each instance of the table and
+# for each of the elements in every row we remove the time element
+# thus we keep only the coordinates.
 
-distances = []
+
+
+for i in range(0,len(test_set)):
+	coords = []
+	for j in test_traj[i]:
+		j.pop(0)
+		coords.append(j)
+	#print coords
+	
+	coords2 = []
+#print train_traj
+
+
+
+
+
+
+
+
 
